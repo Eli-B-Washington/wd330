cf983a4eb86a9fb312ba5acace7e33fdc99d2852
eli-b-washington <bea14018@byui.edu>
2021-02-11 10:29:47 -0800

    changes to challenge 1

--- week6/challenge_1/js/toDos.js
+++ week6/challenge_1/js/toDos.js
@@ -1,52 +1,138 @@
-document.querySelector('form').addEventListener('submit', handleSubmitForm);
-document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
-document.getElementById('clearAll').addEventListener('click', handleClearAll);
+// select everything
+// select the todo-form
+const todoForm = document.querySelector('.todo-form');
+// select the input box
+const todoInput = document.querySelector('.todo-input');
+// select the <ul> with class="todo-items"
+const todoItemsList = document.querySelector('.todo-items');
 
-function handleSubmitForm(e) {
-    e.preventDefault();
-    let input = document.querySelector('input');
-    if (input.value != '')
-        addTodo(input.value);
-    input.value = '';
+// array which stores every todos
+let todos = [];
+
+// add an eventListener on form, and listen for submit event
+todoForm.addEventListener('submit', function(event) {
+  // prevent the page from reloading when submitting the form
+  event.preventDefault();
+  addTodo(todoInput.value); // call addTodo function with input box current value
+});
+
+// function to add todo
+function addTodo(item) {
+  // if item is not empty
+  if (item !== '') {
+    // make a todo object, which has id, name, and completed properties
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false
+    };
+
+    // then add it to todos array
+    todos.push(todo);
+    addToLocalStorage(todos); // then store it in localStorage
+
+    // finally clear the input box value
+    todoInput.value = '';
+  }
 }
 
-function addTodo(todo) {
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
+// function to render given todos to screen
+function renderTodos(todos) {
+  // clear everything inside <ul> with class=todo-items
+  todoItemsList.innerHTML = '';
+
+  // run through each item inside todos
+  todos.forEach(function(item) {
+    // check if the item is completed
+    const checked = item.completed ? 'checked': null;
+
+    // make a <li> element and fill it
+    // <li> </li>
+    const li = document.createElement('li');
+    // <li class="item"> </li>
+    li.setAttribute('class', 'item');
+    // <li class="item" data-key="20200708"> </li>
+    li.setAttribute('data-key', item.id);
+    /* <li class="item" data-key="20200708"> 
+          <input type="checkbox" class="checkbox">
+          Go to Gym
+          <button class="delete-button">X</button>
+        </li> */
+    // if item is completed, then add a class to <li> called 'checked', which will add line-through style
+    if (item.completed === true) {
+      li.classList.add('checked');
+    }
+
     li.innerHTML = `
-        <span class="todo-item">${todo}</span>
-        <button name="checkButton"><i class="fas fa-check-square"></i></button>
-        <button name="deleteButton" ><i class="fas fa-trash"></i></button>
+      <input type="checkbox" class="checkbox" ${checked}>
+      ${item.name}
+      <button class="delete-button">X</button>
     `;
-    li.classList.add('todo-list-item');
-    ul.appendChild(li);
+    // finally add the <li> to the <ul>
+    todoItemsList.append(li);
+  });
+
 }
 
-function handleClickDeleteOrCheck(e) {
-    if (e.target.name == 'checkButton')
-        checkTodo(e);
-
-    if (e.target.name == 'deleteButton')
-        deleteTodo(e);
+// function to add todos to local storage
+function addToLocalStorage(todos) {
+  // conver the array to string then store it.
+  localStorage.setItem('todos', JSON.stringify(todos));
+  // render them to screen
+  renderTodos(todos);
 }
 
-function checkTodo(e) {
-    let item = e.target.parentNode;
-    if (item.style.textDecoration == 'line-through')
-        item.style.textDecoration = 'none';
-    else
-        item.style.textDecoration = 'line-through';
+// function helps to get everything from local storage
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+  // if reference exists
+  if (reference) {
+    // converts back to array and store it in todos array
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
 }
 
-function deleteTodo(e) {
-    let item = e.target.parentNode;
-    
-    item.addEventListener('transitionend', function () {
-        item.remove(); 
-    });
+// toggle the value to completed and not completed
+function toggle(id) {
+  todos.forEach(function(item) {
+    // use == not ===, because here types are different. One is number and other is string
+    if (item.id == id) {
+      // toggle the value
+      item.completed = !item.completed;
+    }
+  });
 
-    item.classList.add('todo-list-item-fall');
+  addToLocalStorage(todos);
 }
-function handleClearAll(e) {
-    document.querySelector('ul').innerHTML = '';
+
+// deletes a todo from todos array, then updates localstorage and renders updated list to screen
+function deleteTodo(id) {
+  // filters out the <li> with the id and updates the todos array
+  todos = todos.filter(function(item) {
+    // use != not !==, because here types are different. One is number and other is string
+    return item.id != id;
+  });
+
+  // update the localStorage
+  addToLocalStorage(todos);
 }
+
+// initially get everything from localStorage
+getFromLocalStorage();
+
+// after that addEventListener <ul> with class=todoItems. Because we need to listen for click event in all delete-button and checkbox
+todoItemsList.addEventListener('click', function(event) {
+  // check if the event is on checkbox
+  if (event.target.type === 'checkbox') {
+    // toggle the state
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+
+  // check if that is a delete-button
+  if (event.target.classList.contains('delete-button')) {
+    // get id from data-key attribute's value of parent <li> where the delete-button is present
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
+  
